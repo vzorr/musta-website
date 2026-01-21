6c776eb2527458b9c572f0243d028d78567b3d1c
vzorr <majid@aibsol.com>
2026-01-21 18:54:01 +0500

    Add Google Analytics to CSP whitelist

--- next.config.js
+++ next.config.js
@@ -76,12 +76,12 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value: "default-src 'self'; " +
-                   "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.google.com https://www.gstatic.com https://www.recaptcha.net; " +
-                   "script-src-elem 'self' 'unsafe-inline' https://www.google.com https://www.gstatic.com https://www.recaptcha.net; " +
+                   "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.google.com https://www.gstatic.com https://www.recaptcha.net https://www.googletagmanager.com; " +
+                   "script-src-elem 'self' 'unsafe-inline' https://www.google.com https://www.gstatic.com https://www.recaptcha.net https://www.googletagmanager.com; " +
                    "frame-src 'self' https://www.google.com https://www.gstatic.com https://www.recaptcha.net; " +
                    "style-src 'self' 'unsafe-inline' https://www.gstatic.com; " +
-                   "img-src 'self' data: blob: https: https://www.gstatic.com; " +
-                   "connect-src 'self' https://www.google.com https://www.gstatic.com https://www.recaptcha.net; " +
+                   "img-src 'self' data: blob: https: https://www.gstatic.com https://www.googletagmanager.com; " +
+                   "connect-src 'self' https://www.google.com https://www.gstatic.com https://www.recaptcha.net https://www.googletagmanager.com https://www.google-analytics.com; " +
                    "font-src 'self' https://fonts.gstatic.com;",
           },
         ],
